710c0e59fa22af9868ab440e3d9a80a5058a4100
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-05-02 08:43:54 +0200

    Inicio

---
+++ src/boletin/pkg26/Boletin26.java
@@ -0,0 +1,16 @@
+package boletin.pkg26;
+
+/**
+ *
+ * @author Adry
+ */
+public class Boletin26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
