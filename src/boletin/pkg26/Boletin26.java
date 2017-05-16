fd2805e7c25b002a0caa83561fa24abdc7660af7
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-05-16 10:11:52 +0200

    Hacer visible la ventana

--- src/boletin/pkg26/Boletin26.java
+++ src/boletin/pkg26/Boletin26.java
@@ -10,7 +10,8 @@ public class Boletin26 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ventana objVen=new Ventana();
+        objVen.setVisible(true);
     }
     
 }
